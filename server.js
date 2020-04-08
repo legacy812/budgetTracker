d5d94e8886b66d5a92a6d12a8af36112b1220f65
Jean Li <legacy812@gmail.com>
2020-04-07 22:20:11 -0700

    packages

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+require('./config')
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch(e => console.error(e));
